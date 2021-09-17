036a4168480763eb474459f4522a8125b662fd82
Admin <Admin@Admin-PC>
2021-09-17 20:51:54 +0530

    This is my second github code

--- src/githubbb/GithubConfig.java
+++ src/githubbb/GithubConfig.java
@@ -3,6 +3,7 @@ package githubbb;
 public class GithubConfig {
 	public static void main(String[] args) {
 		System.out.println("First Git Hub Program");
+		System.out.println("Second Git Hub Program");
 	}
 
 }
